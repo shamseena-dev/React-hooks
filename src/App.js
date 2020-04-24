13887f3ca6e18e073e236ceb7d737ab9633227fd
Shamseena Karumarot <shamseenakarumarot@gmail.com>
2020-04-24 15:18:31 +0200

    Added initial files with todo tasks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
+function Todos({todos}){
+  return(
+  todos.map(todo => (
+    <Todo todo={todo}/>
+  ))
+  )
+}
+function Todo({todo}){
+  return(
+    <div key={todo.id}>
+      <p>{todo.id}</p>
+      <p>{todo.task}</p>
+    </div>
+  )
+}
 function App() {
+  const [todos,setTodos] = useState([
+    {
+      id: 1,
+      task: "Do Exercises"
+    },
+    {
+      id: 2,
+      task: "Practise React courses"
+    },
+    {
+      id: 3,
+      task: "Build Project"
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container center">
+        <div className="card blue-grey">
+        <h2 className="center white-text"> Todo-list Managing App</h2>
+        <Todos todos={todos} />
+        </div>
     </div>
   );
 }
